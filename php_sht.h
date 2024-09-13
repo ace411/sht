8c23f912be909d02bd7dab90ec2ae3dd4eeeb7ca
ace411 <lochbm@gmail.com>
2024-09-13 08:06:53 +0300

    feat: change email address

--- php_sht.h
+++ php_sht.h
@@ -6,7 +6,7 @@ extern zend_module_entry sht_module_entry;
 #define phpext_sht_ptr &sht_module_entry;
 
 #define SHT_VERSION "0.1.0"
-#define SHT_AUTHOR "Lochemem Bruno Michael <lochbm@gmail.com>"
+#define SHT_AUTHOR "Lochemem Bruno Michael <lochbm@live.com>"
 
 #if defined(ZTS) && defined(COMPILE_DL_SHT)
 ZEND_TSRMLS_CACHE_EXTERN()
